a71a056589da80623a15540411aa58e891e210e1
aleTesting <aleksas773@googlemail.com>
2022-12-02 09:51:07 +0000

    small change in code

--- app.js
+++ app.js
@@ -2,43 +2,44 @@ let operators = {
   add: function (num1, num2) {
     try {
       this._checkForNumber(num1, num2);
-    } catch {
-      (e) => console.error(e);
+      // console.log(2);
+      return num1 + num2;
+    } catch (e) {
+      console.error(e);
     }
-    return num1 + num2;
   },
   subtract: function (num1, num2) {
     try {
       this._checkForNumber(num1, num2);
+      return num1 - num2;
     } catch {
       (e) => console.error(e);
     }
-    return num1 - num2;
   },
   multiply: function (num1, num2) {
     try {
       this._checkForNumber(num1, num2);
+      return num1 * num2;
     } catch {
       (e) => console.error(e);
     }
-    return num1 * num2;
   },
   divide: function (num1, num2) {
     try {
-      if (num1 == 0 || num2 == 0) {
+      if (num2 == 0) {
         throw "can't divide by zero";
       }
       this._checkForNumber(num1, num2);
+      return num1 + num2;
     } catch {
       (e) => console.error(e);
     }
-    return num1 + num2;
   },
   _checkForNumber: function (num1, num2) {
-    if (typeof num1 === "number" || typeof num2 === "number") {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
       throw "Parameter is not a number ";
     }
   },
 };
-
-console.log(operators.add(2, 2));
+let result = operators.add(2, 2);
+console.log(result);
